ffa569b0dde06f237019c6a0dda30c47a08b9097
molvqingtai <molvqingtai@gmail.com>
2022-12-05 15:18:07 +0800

    ci: [husky] add husky configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   plugins: [react(), Unocss({})],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
