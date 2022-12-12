69a85abd766976ef19113d6c30c48d0c42fe2dd7
molvqingtai <molvqingtai@gmail.com>
2022-12-12 11:12:45 +0800

    build: update deps and use `@vitejs/plugin-react-swc`

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import Unocss from 'unocss/vite'
 import { resolve } from 'node:path'
 
